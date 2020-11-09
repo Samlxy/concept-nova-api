8f00fbf8a3dd7d5cecdf6154b175df4a630bcd9d
Samlxy <Donsammy265@gmail.com>
2020-11-09 20:12:06 +0100

    Modify App: - display server response on page. - modify project structure.

---
+++ src/component/storage.js
@@ -0,0 +1,19 @@
+// store token from 'POST request server response' after authentication
+export const setUserToken = (token) => sessionStorage.setItem('authToken', token);
+
+// retrieve token for use in viewSiteList GET request
+export const getUserToken = (authToken) => sessionStorage.getItem(authToken);
+
+
+// store site_id from 'POST request server response' after authentication
+export const setSiteId = (site_id) => sessionStorage.setItem('siteID', site_id);
+
+// retrieve site_id for use in viewTankList GET request
+export const getSiteId = (siteID) => sessionStorage.getItem(siteID);
+
+
+// store tank_id from viewTankList GET request 
+export const setTankId = (tank_id) => sessionStorage.setItem('tankID', tank_id);
+
+// retrieve tank_id for use in viewTankDetails
+export const getTankId = (tankID) => sessionStorage.getItem(tankID);
