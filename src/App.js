49186f8024599901f02f7106acda9586d2e28ab7
Samlxy <Donsammy265@gmail.com>
2020-11-10 14:16:59 +0100

    Refactor Code: - unbind methods while using arrow function. - remove trailing lines, add semi-colons.

--- src/App.js
+++ src/App.js
@@ -12,13 +12,9 @@ class Form extends Component {
         this.state = { 
             email: '', 
             password: '',
-        } 
-        /*
-     this.captureEmail = this.captureEmail.bind(this);
-     this.capturePassword = this.capturePassword.bind(this);
-     this.signInAuthentication  = this.signInAuthentication.bind(this);
-     */
-    }
+        }; 
+    };
+
     captureEmail = (event) => this.setState({email: event.target.value});
     
     capturePassword = (event) => this.setState({password: event.target.value});
@@ -27,7 +23,7 @@ class Form extends Component {
         let authEmail = this.state.email;
         let authPassword = this.state.password;
         event.preventDefault();
-        console.log('email: ', authEmail, ' ', 'password:',authPassword)
+        console.log('email: ', authEmail, ' ', 'password:',authPassword);
 
         fetch('https://fcs.concept-nova.com/api/v1/login', {
             method: 'POST',
@@ -39,68 +35,64 @@ class Form extends Component {
                 email: authEmail,
                 password: authPassword
             })
-    
-        }) 
-            .then(response => response.json())
-            .then(response => { 
-                console.log(response)
-                if(response.code === 200) {
-                  alert('You are successfully logged in')
-                } else {
-                  alert('Invalid login credentials')
-                }
-
-            // capture generated token
-            setUserToken(response.message.token)
-            })
-            .catch(err => { 
-                console.log(err) 
-            });
-
-            
-    }
-
-    viewSitesList = () => {  
-        const authToken = getUserToken('authToken')
-        console.log('token: ', authToken)
-
-        window.open(`https://fcs.concept-nova.com/api/v1/sites/?token=${authToken}`)
-
-        fetch(`https://fcs.concept-nova.com/api/v1/sites/?token=${authToken}`) 
+        })
         .then(response => response.json())
         .then(response => { 
-            console.log(response)
+            console.log(response);
+            if(response.code === 200) {
+                alert('You are successfully logged in');
+            } else {
+                alert('Invalid login credentials');
+            };
+        // capture generated token
+        setUserToken(response.message.token);
+        })
+        .catch(err => { 
+            console.log(err);
+        });      
+    };
+
+    viewSitesList = () => {  
+        const authToken = getUserToken('authToken');
+        console.log('token: ', authToken);
+
+        window.open(`https://fcs.concept-nova.com/api/v1/sites/?token=${authToken}`);
+
+        fetch(`https://fcs.concept-nova.com/api/v1/sites/?token=${authToken}`)
+        .then(response => response.json())
+        .then(response => { 
+            console.log(response);
 
         // capture site_id
-        setSiteId(response.message[0].site_id) 
+        setSiteId(response.message[0].site_id);
         })
         .catch(err => { 
             console.log(err) 
-        }) 
-    } 
+        });
+    };
 
     viewTanksList = () => {
-        const authToken = getUserToken('authToken')
+        const authToken = getUserToken('authToken');
         const siteID = getSiteId('siteID');
-        console.log('site_id: ', siteID)
+        console.log('site_id: ', siteID);
 
-        window.open(`https://fcs.concept-nova.com/api/v1/sites/${siteID}?token=${authToken}`)
+        window.open(`https://fcs.concept-nova.com/api/v1/sites/${siteID}?token=${authToken}`);
 
         fetch(`https://fcs.concept-nova.com/api/v1/sites/${siteID}?token=${authToken}`) 
         .then(response => response.json())
         .then(response => { 
-            console.log(response)
+            console.log(response);
             
         // capture tank_id
-        setTankId(response.message[0].tank_id) 
+        setTankId(response.message[0].tank_id); 
         })
         .catch(err => { 
             console.log(err) 
-        }) 
-    }
+        }); 
+    };
 
     viewTankDetails = () => {
-        const authToken = getUserToken('authToken')
+        const authToken = getUserToken('authToken');
         const siteID = getSiteId('siteID');
         const tankID = getTankId('tankID');
         console.log('tank_id: ', tankID);
@@ -110,21 +102,22 @@ class Form extends Component {
         fetch(`https://fcs.concept-nova.com/api/v1/sites/${siteID}/${tankID}?token=${authToken}`) 
         .then(response => response.json())
         .then(response => { 
-            console.log(response)
+            console.log(response);
         })
         .catch(err => { 
             console.log(err) 
-        }) 
-    }
+        }); 
+    };
 
     render () {
         return (
             <div id='form'>
-                <img src={conceptNova} alt='concept nova logo'/>
+                <img src={conceptNova} alt='concept-nova-logo'/>
                 <h2>Sign In</h2>
+
                 <form>
-                    <input className='input-style' id='email-field' type='email' size='32' placeholder='Email address*' onChange={this.captureEmail}/><p></p>
-                    <input className='input-style' id='password-field' type='password' size='32' placeholder='Password *' onChange={this.capturePassword}/>
+                    <input className='input-style' id='email-field' type='email' size='32' placeholder='Email address *' onChange={this.captureEmail} /><p></p>
+                    <input className='input-style' id='password-field' type='password' size='32' placeholder='Password *' onChange={this.capturePassword} />
                 </form>
 
                 <button id='sign-in' onClick={this.signInAuthentication}>Sign In</button><br />
@@ -132,8 +125,8 @@ class Form extends Component {
                 <button className='list-sites-tanks' onClick={this.viewTanksList}>View Tanks</button>
                 <button id='details' onClick={this.viewTankDetails}>Tank Details</button>
             </div>
-        )
-    }
-}
+        );
+    };
+};
 
 export default Form;
--- src/component/storage.js
+++ src/component/storage.js
@@ -1,19 +1,19 @@
 // store token from 'POST request server response' after authentication
 export const setUserToken = (token) => sessionStorage.setItem('authToken', token);
 
-// retrieve token for use in viewSiteList GET request
+// retrieve token for use in 'viewSiteList GET request'
 export const getUserToken = (authToken) => sessionStorage.getItem(authToken);
 
 
-// store site_id from 'POST request server response' after authentication
+// store site_id from 'GET request server response' after the use of generated token
 export const setSiteId = (site_id) => sessionStorage.setItem('siteID', site_id);
 
-// retrieve site_id for use in viewTankList GET request
+// retrieve site_id for use in 'viewTankList GET request'
 export const getSiteId = (siteID) => sessionStorage.getItem(siteID);
 
 
-// store tank_id from viewTankList GET request 
+// store tank_id from 'viewTankList GET request'
 export const setTankId = (tank_id) => sessionStorage.setItem('tankID', tank_id);
 
-// retrieve tank_id for use in viewTankDetails
+// retrieve tank_id for use in 'viewTankDetails GET request'
 export const getTankId = (tankID) => sessionStorage.getItem(tankID);
